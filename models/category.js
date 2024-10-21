feed3d51c059213a285210b58d95bc88e2715454
Jackson Cassemiro <jacksondgls@live.com>
2024-10-21 19:58:54 -0300

    fix: correções e migração
    Signed-off-by: Jackson Cassemiro <jacksondgls@live.com>

--- models/category.js
+++ models/category.js
@@ -2,23 +2,43 @@
 const {
   Model
 } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Category extends Model {
     static associate(models) {
+      // Define association here
       Category.hasMany(models.Product, {
         foreignKey: 'categoryId',
-        onDelete: 'CASCADE',
       });
     }
   }
+
   Category.init(
     {
-      name: DataTypes.STRING,
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      }
     },
     {
       sequelize,
       modelName: 'Category',
     }
   );
+
   return Category;
 };
--- models/product.js
+++ models/product.js
@@ -2,6 +2,7 @@
 const {
   Model
 } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Product extends Model {
     static associate(models) {
@@ -13,17 +14,46 @@ module.exports = (sequelize, DataTypes) => {
 
   Product.init(
     {
-      name: DataTypes.STRING,
-      quantity: DataTypes.INTEGER,
-      inStock: DataTypes.BOOLEAN,
-      productImage: DataTypes.STRING,
-      price: DataTypes.INTEGER,
-      expiryDate: DataTypes.DATE,
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false, // Se o nome é obrigatório
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false, // Se a quantidade é obrigatória
+      },
+      inStock: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false, // Se o status em estoque é obrigatório
+      },
+      productImage: {
+        type: DataTypes.STRING,
+        allowNull: true, // Se a imagem do produto não é obrigatória
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false, // Isso deve ser verdadeiro se o preço é obrigatório
+      },
+      expiryDate: {
+        type: DataTypes.DATE,
+        allowNull: true, // Se a data de validade não é obrigatória
+      },
+      categoryId: {
+        type: DataTypes.UUID,
+        allowNull: false, // Se a categoria é obrigatória
+      },
     },
     {
       sequelize,
       modelName: 'Product',
     }
   );
+
   return Product;
 };
